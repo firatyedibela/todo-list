d62ea4eaf68071002a24bcb85c71fb4066c919d0
Firat Yedibela <yedibelafirat0@gmail.com>
2024-03-24 15:10:55 +0300

    Add projects list and logic

--- src/scripts/model.js
+++ src/scripts/model.js
@@ -1,13 +1,23 @@
 export default class Todo {
   static list = [];
+  static projects = [];
 
-  static addTodo(title, description, dueDate, priority) {
+  static addTodo(title, description, dueDate, priority, project) {
     Todo.list.push({title, description, dueDate, priority});
   }
 
   static removeTodo(title) {
     const idx = Todo.list.findIndex(todo => todo.title === title);
     Todo.list.splice(idx, 1);
+  }
+
+  static addProject(name) {
+    Todo.projects.push({name, tasks: []})
+  }
+
+  static removeProject(name) {
+    const idx = Todo.projects.findIndex(project => project.name === name);
+    Todo.projects.splice(idx, 1);
   }
 }
 
