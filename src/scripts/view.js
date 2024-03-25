0bd9b34d526e3ca605f46804cd7a17b243d7ecdb
Firat Yedibela <yedibelafirat0@gmail.com>
2024-03-25 11:06:06 +0300

    Add new-task and cancel-task logic

--- src/scripts/view.js
+++ src/scripts/view.js
@@ -1,3 +1,5 @@
+import { submitTask } from '../index.js';
+
 export default class View {
   static renderTaskForm(projects) {
     // Remove the existing task form
@@ -9,19 +11,19 @@ export default class View {
       <form class="task-form" action="">
         <div>
           <label for="title">Title</label>
-          <input type="text" name="title" id="title">
+          <input class="task-input" type="text" name="title" id="title">
         </div>
         <div>
           <label for="description">Description</label>
-          <input type="text" name="description" id="description">
+          <input class="task-input" type="text" name="description" id="description">
         </div>
         <div>
           <label for="dueDate">Due Date</label>
-          <input type="text" name="dueDate" id="dueDate">
+          <input class="task-input" type="text" name="dueDate" id="dueDate">
         </div>
         <div>
           <label for="priority">Priority</label>
-          <select name="priority" id="priority">
+          <select class="task-input" name="priority" id="priority">
             <option value="low">Low</option>
             <option value="normal">Normal</option>
             <option value="high">High</option>
@@ -29,7 +31,7 @@ export default class View {
         </div>
         <div>
           <label for="task-project">Project</label>
-          <select name="task-project" id="task-project">
+          <select class="task-input" name="task-project" id="task-project">
             <option value="none" selected>Select a project</option>
           </select>
         </div>
@@ -43,14 +45,15 @@ export default class View {
     formContainer.querySelector('.submit-new-task-btn')
       .addEventListener('click', (e) => {
         e.preventDefault();
-        handleSubmitTask()
+        submitTask()
+        View.removeTaskForm();
       }
     );
 
     formContainer.querySelector('.cancel-task-button')
       .addEventListener('click', (e) => {
         e.preventDefault();
-        handleCancelTask();
+        View.removeTaskForm();
       }
     );
 
